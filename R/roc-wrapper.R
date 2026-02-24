85b9e2ac511f771ba393a7a68b4264566eff013c
lauriekell <laurie@kell.es>
2026-02-24 08:34:33 +0000

    roc

---
+++ R/roc-wrapper.R
@@ -0,0 +1,21 @@
+#' ROC (Receiver Operating Characteristic) Analysis
+#'
+#' Wrapper function for ROC analysis from FLSkill package.
+#' This function provides a convenient interface to FLSkill's \code{roc2} function.
+#'
+#' @param ... Arguments passed to \code{FLSkill::roc2}
+#'
+#' @return Results from ROC analysis
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Use roc function (wraps FLSkill::roc2)
+#' roc_result <- roc(indicator, reference_status)
+#' }
+#'
+#' @seealso \code{\link[FLSkill]{roc2}}
+roc <- function(...) {
+  FLSkill::roc2(...)
+}
